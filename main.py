35d7dfccafad21bccf120bd2132638c003dcedad
Eunji.Heo <eunja.heo@samsung.com>
2024-06-13 16:25:53 +0900

    [refactoring] extract test_same_length, remove unnecessary code

--- main.py
+++ main.py
@@ -5,12 +5,11 @@ class SimilarityChecker:
     def check_length_similarity(self, str1, str2):
         length1 = len(str1)
         length2 = len(str2)
+
         if length2 > length1:
             length1, length2 = length2, length1
 
-        if length1 == length2:
-            return 60
-        elif length1 > length2 * 2:
+        if length1 > length2 * 2:
             return 0
 
         return int((1 - ((length1 - length2) / length2)) * 60)
--- test_main.py
+++ test_main.py
@@ -8,21 +8,18 @@ class TestSimilarityChecker(TestCase):
         self.checker = SimilarityChecker()
 
     def test_same_length(self):
-        ret = self.checker.check_length_similarity("ABC", "ABC")
-        self.assertEqual(ret, 60)
+        self.assert_matched_result("ABC", "ABC", 60)
 
     def test_over_than_twice_length(self):
-        ret = self.checker.check_length_similarity("A", "ABC")
-        self.assertEqual(ret, 0)
-        ret = self.checker.check_length_similarity("ABC", "A")
-        self.assertEqual(ret, 0)
+        self.assert_matched_result("A", "ABC", 0)
+        self.assert_matched_result("ABC", "A", 0)
 
     def test_different_length(self):
-        ret = self.checker.check_length_similarity("ABCDE", "ABC")
-        self.assertEqual(ret, 20)
-        ret = self.checker.check_length_similarity("ABC", "ABCDE")
-        self.assertEqual(ret, 20)
-        ret = self.checker.check_length_similarity("ABCE", "ABC")
-        self.assertEqual(ret, 40)
-        ret = self.checker.check_length_similarity("ABCDEFDDG", "ABCASGD")
-        self.assertEqual(ret, 42)
+        self.assert_matched_result("ABCDE", "ABC", 20)
+        self.assert_matched_result("ABC", "ABCDE", 20)
+        self.assert_matched_result("ABCE", "ABC", 40)
+        self.assert_matched_result("ABCDEFDDG", "ABCASGD", 42)
+
+    def assert_matched_result(self, str1, str2, score):
+        ret = self.checker.check_length_similarity(str1, str2)
+        self.assertEqual(ret, score)
