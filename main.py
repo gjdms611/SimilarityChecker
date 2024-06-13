dbc377922316c42f84967fda730d5d21334db1f9
Eunji.Heo <eunja.heo@samsung.com>
2024-06-13 15:52:27 +0900

    [feature] add return 0 when the difference in string length is more than 2 times

--- main.py
+++ main.py
@@ -3,5 +3,10 @@ class SimilarityChecker:
         pass
 
     def check_length_similarity(self, param, param1):
-        return 60
-
+        length1 = len(param)
+        length2 = len(param1)
+        if length1 == length2:
+            return 60
+        elif length1 * 2 < length2 or length1 > length2 * 2:
+            return 0
+        return 0
--- test_main.py
+++ test_main.py
@@ -10,3 +10,9 @@ class TestSimilarityChecker(TestCase):
     def test_same_length(self):
         ret = self.checker.check_length_similarity("ABC", "ABC")
         self.assertEqual(ret, 60)
+
+    def test_over_than_twice_length(self):
+        ret = self.checker.check_length_similarity("A", "ABC")
+        self.assertEqual(ret, 0)
+        ret = self.checker.check_length_similarity("ABC", "A")
+        self.assertEqual(ret, 0)
