e77ceab761ea4ad3c5618e5eb40a9b4482534bcd
Eunji.Heo <eunja.heo@samsung.com>
2024-06-13 15:47:24 +0900

    [refactoring] add setup in test

--- test_main.py
+++ test_main.py
@@ -4,7 +4,9 @@ from main import SimilarityChecker
 
 
 class TestSimilarityChecker(TestCase):
-    def test_same_length(self):
+    def setUp(self):
         self.checker = SimilarityChecker()
+
+    def test_same_length(self):
         ret = self.checker.check_length_similarity("ABC", "ABC")
         self.assertEqual(ret, 60)
