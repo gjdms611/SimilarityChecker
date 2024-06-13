73edc7448a5e3e141d788a6212c29b6e92a50304
Eunji.Heo <eunja.heo@samsung.com>
2024-06-13 15:46:36 +0900

    [feature] add length check logic for same length

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
 class SimilarityChecker:
-    pass
+    def __init__(self):
+        pass
+
+    def check_length_similarity(self, param, param1):
+        return 60
+
--- test_main.py
+++ test_main.py
@@ -1,5 +1,10 @@
 from unittest import TestCase
 
+from main import SimilarityChecker
+
 
 class TestSimilarityChecker(TestCase):
-    pass
+    def test_same_length(self):
+        self.checker = SimilarityChecker()
+        ret = self.checker.check_length_similarity("ABC", "ABC")
+        self.assertEqual(ret, 60)
