f8bc14240c4b944669e61dc3d3ada9d60b31b08b
Eunji.Heo <eunja.heo@samsung.com>
2024-06-13 15:54:28 +0900

    [refactoring] change value name, generalize code

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ class SimilarityChecker:
     def __init__(self):
         pass
 
-    def check_length_similarity(self, param, param1):
-        length1 = len(param)
-        length2 = len(param1)
+    def check_length_similarity(self, str1, str2):
+        length1 = len(str1)
+        length2 = len(str2)
+        if length2 > length1:
+            length1, length2 = length2, length1
+
         if length1 == length2:
             return 60
-        elif length1 * 2 < length2 or length1 > length2 * 2:
+        elif length1 * 2 < length2:
             return 0
         return 0
